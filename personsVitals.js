8732a2b1dbe59eb1db6d84af8d688daa106c7b50
Michael Truss <badmoonrising31@gmail.com>
2019-01-07 14:31:29 -0600

    Create personsVitals.js

---
+++ personsVitals.js
@@ -0,0 +1,41 @@
+var years = [1990, 1946, 2005, 1980, 1981];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++;) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2018 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+       return Math.round(206.9 - (0.67 * el));
+    } else {
+       return -1;
+    }
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
+
+
+
+
+
+
+
+
